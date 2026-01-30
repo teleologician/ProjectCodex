f5f03ec5b2238151ea65754a703f24c62652eb0b
teleologician <thomaswright1318@gmail.com>
2026-01-29 22:27:11 -0500

    Sprint 1 Complete

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 #include <glad/glad.h>
 #include <SDL2/SDL.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace {
 GLuint CompileShader(GLenum type, const char* source) {
@@ -51,7 +54,7 @@ int main() {
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
     SDL_Window* window = SDL_CreateWindow(
-        "Hello Triangle",
+        "Vampire Diary Prototype",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         800,
@@ -79,20 +82,25 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    glViewport(0, 0, 800, 600);
+    int window_width = 800;
+    int window_height = 600;
+    glViewport(0, 0, window_width, window_height);
+    glEnable(GL_DEPTH_TEST);
 
     const char* vertex_source =
         "#version 330 core\n"
-        "layout (location = 0) in vec2 aPos;\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "uniform mat4 uMVP;\n"
         "void main() {\n"
-        "    gl_Position = vec4(aPos, 0.0, 1.0);\n"
+        "    gl_Position = uMVP * vec4(aPos, 1.0);\n"
         "}\n";
 
     const char* fragment_source =
         "#version 330 core\n"
         "out vec4 FragColor;\n"
+        "uniform vec3 uColor;\n"
         "void main() {\n"
-        "    FragColor = vec4(0.92, 0.16, 0.20, 1.0);\n"
+        "    FragColor = vec4(uColor, 1.0);\n"
         "}\n";
 
     GLuint vs = CompileShader(GL_VERTEX_SHADER, vertex_source);
@@ -114,49 +122,170 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    float vertices[] = {
-        -0.6f, -0.4f,
-         0.6f, -0.4f,
-         0.0f,  0.6f
+    float ground_vertices[] = {
+        -10.0f, 0.0f, -10.0f,
+         10.0f, 0.0f, -10.0f,
+         10.0f, 0.0f,  10.0f,
+
+        -10.0f, 0.0f, -10.0f,
+         10.0f, 0.0f,  10.0f,
+        -10.0f, 0.0f,  10.0f
     };
 
-    GLuint vao = 0;
-    GLuint vbo = 0;
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+    float cube_vertices[] = {
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f
+    };
+
+    GLuint ground_vao = 0;
+    GLuint ground_vbo = 0;
+    glGenVertexArrays(1, &ground_vao);
+    glGenBuffers(1, &ground_vbo);
+    glBindVertexArray(ground_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, ground_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(ground_vertices), ground_vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    GLuint cube_vao = 0;
+    GLuint cube_vbo = 0;
+    glGenVertexArrays(1, &cube_vao);
+    glGenBuffers(1, &cube_vbo);
+    glBindVertexArray(cube_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, cube_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    GLint mvp_location = glGetUniformLocation(program, "uMVP");
+    GLint color_location = glGetUniformLocation(program, "uColor");
+
+    glm::vec3 player_position(0.0f, 0.0f, 0.0f);
+    const float player_speed = 4.0f;
+    const glm::vec3 camera_offset(0.0f, 5.0f, 7.0f);
+
+    Uint64 last_ticks = SDL_GetPerformanceCounter();
     bool running = true;
     while (running) {
+        Uint64 current_ticks = SDL_GetPerformanceCounter();
+        float delta_time = static_cast<float>(current_ticks - last_ticks) /
+                           static_cast<float>(SDL_GetPerformanceFrequency());
+        last_ticks = current_ticks;
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 running = false;
             } else if (event.type == SDL_WINDOWEVENT &&
                        event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
-                glViewport(0, 0, event.window.data1, event.window.data2);
+                window_width = event.window.data1;
+                window_height = event.window.data2;
+                glViewport(0, 0, window_width, window_height);
             }
         }
 
-        glClearColor(0.06f, 0.06f, 0.10f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        const Uint8* keys = SDL_GetKeyboardState(nullptr);
+        glm::vec3 move(0.0f);
+        if (keys[SDL_SCANCODE_W] || keys[SDL_SCANCODE_UP]) {
+            move.z -= 1.0f;
+        }
+        if (keys[SDL_SCANCODE_S] || keys[SDL_SCANCODE_DOWN]) {
+            move.z += 1.0f;
+        }
+        if (keys[SDL_SCANCODE_A] || keys[SDL_SCANCODE_LEFT]) {
+            move.x -= 1.0f;
+        }
+        if (keys[SDL_SCANCODE_D] || keys[SDL_SCANCODE_RIGHT]) {
+            move.x += 1.0f;
+        }
+        if (glm::length(move) > 0.0f) {
+            move = glm::normalize(move);
+        }
+        player_position += move * player_speed * delta_time;
+
+        glClearColor(0.05f, 0.05f, 0.08f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(program);
-        glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glm::mat4 projection = glm::perspective(
+            glm::radians(60.0f),
+            static_cast<float>(window_width) / static_cast<float>(window_height),
+            0.1f,
+            100.0f);
+        glm::mat4 view = glm::lookAt(
+            player_position + camera_offset,
+            player_position,
+            glm::vec3(0.0f, 1.0f, 0.0f));
+
+        glm::mat4 ground_model(1.0f);
+        glm::mat4 ground_mvp = projection * view * ground_model;
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(ground_mvp));
+        glUniform3f(color_location, 0.12f, 0.16f, 0.18f);
+        glBindVertexArray(ground_vao);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+
+        glm::mat4 player_model = glm::translate(
+            glm::mat4(1.0f),
+            player_position + glm::vec3(0.0f, 0.5f, 0.0f));
+        glm::mat4 player_mvp = projection * view * player_model;
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(player_mvp));
+        glUniform3f(color_location, 0.92f, 0.16f, 0.20f);
+        glBindVertexArray(cube_vao);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
 
         SDL_GL_SwapWindow(window);
     }
 
-    glDeleteVertexArrays(1, &vao);
-    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &ground_vao);
+    glDeleteBuffers(1, &ground_vbo);
+    glDeleteVertexArrays(1, &cube_vao);
+    glDeleteBuffers(1, &cube_vbo);
     glDeleteProgram(program);
 
     SDL_GL_DeleteContext(gl_context);
